879fc5efb3c814019ca46b304104a36c74604ab3
Vinicius Rocha <rochaa.vinicius@outlook.com>
2021-07-05 20:03:09 -0300

    Adicionando consumo put e delete tema

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -20,8 +20,22 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogpessoalwebv.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessoalwebv.herokuapp.com/tema/${id}`, this.token)
+
+  }
+
   postTema(tema: Tema): Observable<Tema>{
     return this.http.post<Tema>('https://blogpessoalwebv.herokuapp.com/tema', tema, this.token)
 
   }
+  
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessoalwebv.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://blogpessoalwebv.herokuapp.com/tema/${id}`, this.token)
+  }
+  
 }
