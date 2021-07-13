6260a60d507f88564e6bcdb3cd821449b665f173
Vinicius Rocha <rochaa.vinicius@outlook.com>
2021-07-13 10:14:44 -0300

    Implementando Pesquisas

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -19,10 +19,12 @@ export class InicioComponent implements OnInit {
 
   postagem: Postagem = new Postagem()
   listaPostagens: Postagem[]
+  tituloPost: string
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
   idTema: number
+  nomeTema: string
 
   user: User = new User()
   idUser = environment.id
@@ -87,4 +89,24 @@ export class InicioComponent implements OnInit {
       this.getAllPostagens()
     })
   }
+
+  findByTituloPostagem() {
+    if (this.tituloPost == '') {
+      this.getAllPostagens()
+    } else {
+      this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp: Postagem[]) =>{ this.listaPostagens = resp
+      })
+    }
+  }
+
+  findByNomeTema(){
+    if(this.nomeTema == ''){
+      this.getAllTemas()
+    } else{
+      this.temaService.getByNomeTema(this.nomeTema).subscribe((resp: Tema[])=> {
+        return this.listaTemas = resp
+      })
+    }
+  }
+  
 }
