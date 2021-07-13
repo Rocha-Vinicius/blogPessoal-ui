638d66e90e78db269486a052d3b64cf21d0d6b54
Vinicius Rocha <rochaa.vinicius@outlook.com>
2021-07-12 23:22:15 -0300

    Implementação do put usuario

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -34,6 +34,8 @@ export class InicioComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    window.scroll(0,0)
+    
     if (environment.token == "") {
       this.router.navigate(["/entrar"])
     }
